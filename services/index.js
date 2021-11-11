125b2bf93e6b139887ffdd8db100391d03d00e59
yashar6846 <yasharnajafi6846@gmail.com>
2021-11-11 12:30:07 +0300

    practices graphqlcms

---
+++ services/index.js
@@ -0,0 +1,37 @@
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            auther {
+              bio
+              name
+              phtho {
+                url
+              }
+              id
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuerdimage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.postsConnection.edges;
+};
